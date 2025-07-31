8dd2c3215a2b68e2fd37b7977d46fce1dd713072
nxsag100301 <nguyenxuansang10032001@gmail.com>
2025-07-31 11:34:31 +0700

    fix-reload-v2

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '~', replacement: '/src' }]
